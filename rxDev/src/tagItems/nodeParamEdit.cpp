6f6e771f66e0029869d4a6f297c9d95db491818f
F.Muellers@gmail.com <F.Muellers@gmail.com@1a7593b6-376c-c873-a36f-2cc06b3c3c74>
2011-10-15 12:17:29 +0000

    files for the nodeParam gui

---
+++ rxDev/src/tagItems/nodeParamEdit.cpp
@@ -0,0 +1,71 @@
+#include "ui_nodeParamEdit.h"
+#include "nodeParamEdit.h"
+#include <QMessageBox>
+
+NodeParamEdit::NodeParamEdit(QStringList list, QWidget *parent) : QDialog(parent), ui(new Ui::NodeParamEdit) {
+    ui->setupUi(this);
+    selectedParam=-1;
+    paramModel = new QStandardItemModel(list.count(),3);
+    paramModel->setHeaderData(0,Qt::Horizontal,QObject::tr("topic"));
+    paramModel->setHeaderData(1,Qt::Horizontal,QObject::tr("type"));
+    paramModel->setHeaderData(2,Qt::Horizontal,QObject::tr("default"));
+
+    for (int row = 0; row < list.count(); ++row) {
+        QStringList tempList;
+        QString tempString;
+        tempList=list.at(row).split(QRegExp("[\{\[]"));
+        tempString=tempList.at(0);
+        QStandardItem *item0 = new QStandardItem(tempString);
+        paramModel->setItem(row,0,item0);
+        tempString=tempList.at(1);
+        tempString.chop(1);
+        item0 = new QStandardItem(QString(tempString));
+        paramModel->setItem(row,1,item0);
+        if (tempList.count()==3){
+            tempString=tempList.at(2);
+            tempString.chop(1);
+            item0 = new QStandardItem(QString(tempString));
+        }else
+            item0 = new QStandardItem(QString("-"));
+        paramModel->setItem(row,2,item0);
+
+        }
+
+    ui->tableView_nodeParam->setModel(paramModel);
+    connect(ui->tableView_nodeParam,SIGNAL(clicked(QModelIndex)), this, SLOT(selectionHandle_paramItem(const QModelIndex &)));
+
+}
+
+NodeParamEdit::~NodeParamEdit() {
+    delete ui;
+}
+void NodeParamEdit::selectionHandle_paramItem(const QModelIndex &index)
+{
+    if (index.isValid()){   //prevents index out of bounds errors
+        selectedParam=index.row();
+    }
+}
+void NodeParamEdit::reject() {
+
+    QDialog::reject();
+}
+
+void NodeParamEdit::accept() {
+
+
+    if ( selectedParam==-1){
+        QMessageBox::information(this, (QString::fromUtf8("Information")),
+                                 QString::fromUtf8("<h2>Param missing</h2>"
+                                                   "<p>Please choose a parameter!</p>"));
+
+    } else{
+
+        QDialog::accept();
+    }
+
+}
+
+int NodeParamEdit::getParam(){
+
+        return selectedParam;
+}
