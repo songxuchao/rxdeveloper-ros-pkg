7f59fea48f290f79c55a6a1c22916bc6af637a03
F.Muellers@gmail.com <F.Muellers@gmail.com@1a7593b6-376c-c873-a36f-2cc06b3c3c74>
2011-11-20 11:28:58 +0000

    bugfixes for the last commit

--- rxDev/src/tagItems/remapArrowEdit.cpp
+++ rxDev/src/tagItems/remapArrowEdit.cpp
@@ -62,24 +62,81 @@ RemapArrowEdit::RemapArrowEdit(RemapArrow *item, QWidget *parent) : QDialog(pare
 
     ui->lineEdit_if->setText(item->getIf());
     ui->lineEdit_unless->setText(item->getUnless());
-    model_start_subscribe = new QStringListModel;
-    model_start_subscribe->setStringList(item->startItem()->getSubscriptions());
-    ui->listView_startNode_subscribe->setModel(model_start_subscribe);
-    model_end_publish = new QStringListModel;
-    model_end_publish->setStringList(item->endItem()->getPublications());
-    ui->listView_endNode_publish->setModel(model_end_publish);
-    model_start_publish = new QStringListModel;
-    model_start_publish->setStringList(item->startItem()->getPublications());
-    ui->listView_startNode_publish->setModel(model_start_publish);
-    model_end_subscribe = new QStringListModel;
-    model_end_subscribe->setStringList(item->endItem()->getSubscriptions());
-    ui->listView_endNode_subscribe->setModel(model_end_subscribe);
+
+    model_start_subscribe = new QStandardItemModel(item->startItem()->getSubscriptions().size(),2);
+    model_start_subscribe->setHeaderData(0,Qt::Horizontal,QObject::tr("topic"));
+    model_start_subscribe->setHeaderData(1,Qt::Horizontal,QObject::tr("type"));
+    ui->tableView_startNode_subscribe->setModel(model_start_subscribe);
+    ui->tableView_startNode_subscribe->setColumnWidth(0,167);
+    ui->tableView_startNode_subscribe->setColumnWidth(1,167);
+    for (int i=0;i<item->startItem()->getSubscriptions().count();i++){
+        QStringList sub = item->startItem()->getSubscriptions().at(i).split(" ");
+        QStandardItem *item0;
+
+         item0 = new QStandardItem(sub.at(0));
+         model_start_subscribe->setItem(i,0,item0);
+         item0 = new QStandardItem(sub.at(1));
+         model_start_subscribe->setItem(i,1,item0);
+    }
+
+    model_end_publish= new QStandardItemModel(item->endItem()->getPublications().size(),2);
+    model_end_publish->setHeaderData(0,Qt::Horizontal,QObject::tr("topic"));
+    model_end_publish->setHeaderData(1,Qt::Horizontal,QObject::tr("type"));
+    ui->tableView_endNode_publish->setModel(model_end_publish);
+    ui->tableView_endNode_publish->setColumnWidth(0,167);
+    ui->tableView_endNode_publish->setColumnWidth(1,167);
+    for (int i=0;i<item->endItem()->getPublications().count();i++){
+        QStringList pub = item->endItem()->getPublications().at(i).split(" ");
+        QStandardItem *item0;
+
+         item0 = new QStandardItem(pub.at(0));
+         model_end_publish->setItem(i,0,item0);
+         item0 = new QStandardItem(pub.at(1));
+         model_end_publish->setItem(i,1,item0);
+    }
+
+    model_start_publish = new QStandardItemModel(item->startItem()->getPublications().size(),2);
+    model_start_publish ->setHeaderData(0,Qt::Horizontal,QObject::tr("topic"));
+    model_start_publish ->setHeaderData(1,Qt::Horizontal,QObject::tr("type"));
+    ui->tableView_startNode_publish->setModel(model_start_publish);
+    ui->tableView_startNode_publish->setColumnWidth(0,167);
+    ui->tableView_startNode_publish->setColumnWidth(1,167);
+    for (int i=0;i<item->startItem()->getPublications().count();i++){
+        QStringList pub = item->startItem()->getPublications().at(i).split(" ");
+        QStandardItem *item0;
+
+         item0 = new QStandardItem(pub.at(0));
+         model_start_publish->setItem(i,0,item0);
+         item0 = new QStandardItem(pub.at(1));
+         model_start_publish->setItem(i,1,item0);
+    }
+
+    model_end_subscribe= new QStandardItemModel(item->endItem()->getSubscriptions().size(),2);
+    model_end_subscribe->setHeaderData(0,Qt::Horizontal,QObject::tr("topic"));
+    model_end_subscribe->setHeaderData(1,Qt::Horizontal,QObject::tr("type"));
+    ui->tableView_endNode_subscribe->setModel(model_end_subscribe);
+    ui->tableView_endNode_subscribe->setColumnWidth(0,167);
+    ui->tableView_endNode_subscribe->setColumnWidth(1,167);
+    for (int i=0;i<item->endItem()->getPublications().count();i++){
+        QStringList sup = item->endItem()->getPublications().at(i).split(" ");
+        QStandardItem *item0;
+
+         item0 = new QStandardItem(sup.at(0));
+         model_end_subscribe->setItem(i,0,item0);
+         item0 = new QStandardItem(sup.at(1));
+         model_end_subscribe->setItem(i,1,item0);
+    }
+
+
     ui->lineEdit_from->setText(item->getFrom());
     ui->lineEdit_to->setText(item->getTo());
-    connect(ui->listView_startNode_subscribe->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_start_subscribe(const QItemSelection &,const QItemSelection &)));
-    connect(ui->listView_endNode_publish->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_end_publish(const QItemSelection &,const QItemSelection &)));
-    connect(ui->listView_startNode_publish->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_start_publish(const QItemSelection &,const QItemSelection &)));
-    connect(ui->listView_endNode_subscribe->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_end_subscribe(const QItemSelection &,const QItemSelection &)));
+
+
+
+    connect(ui->tableView_startNode_subscribe->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_start_subscribe(const QItemSelection &,const QItemSelection &)));
+    connect(ui->tableView_endNode_publish->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_end_publish(const QItemSelection &,const QItemSelection &)));
+    connect(ui->tableView_startNode_publish->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_start_publish(const QItemSelection &,const QItemSelection &)));
+    connect(ui->tableView_endNode_subscribe->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &,const QItemSelection &)), this, SLOT(selectionHandle_end_subscribe(const QItemSelection &,const QItemSelection &)));
 }
 
 RemapArrowEdit::~RemapArrowEdit() {
@@ -87,52 +144,42 @@ RemapArrowEdit::~RemapArrowEdit() {
 }
 void RemapArrowEdit::selectionHandle_start_subscribe(const QItemSelection &selected, const QItemSelection &deselected)
 {
-    ui->listView_startNode_publish->selectionModel()->clearSelection();
-    ui->listView_endNode_subscribe->selectionModel()->clearSelection();
+    ui->tableView_startNode_publish->selectionModel()->clearSelection();
+    ui->tableView_endNode_subscribe->selectionModel()->clearSelection();
     QModelIndex index;
-    index =  ui->listView_startNode_subscribe->selectionModel()->currentIndex();
+    index =  ui->tableView_startNode_subscribe->selectionModel()->currentIndex();
     fromString=(index.data(Qt::DisplayRole).toString());
-    int chopIndex = fromString.indexOf("{");
-    fromString.chop(fromString.count()-chopIndex);
     updateFrom();
 }
 
 
 void RemapArrowEdit::selectionHandle_end_publish(const QItemSelection &selected, const QItemSelection &deselected)
 {
-    ui->listView_startNode_publish->selectionModel()->clearSelection();
-    ui->listView_endNode_subscribe->selectionModel()->clearSelection();
+    ui->tableView_startNode_publish->selectionModel()->clearSelection();
+    ui->tableView_endNode_subscribe->selectionModel()->clearSelection();
     QModelIndex index;
-    index =  ui->listView_endNode_publish->selectionModel()->currentIndex();
+    index =  ui->tableView_endNode_publish->selectionModel()->currentIndex();
     toString =( index.data(Qt::DisplayRole).toString());
-    int chopIndex = toString.indexOf("{");
-    toString.chop(toString.count()-chopIndex);
-
     updateTo();
 }
 void RemapArrowEdit::selectionHandle_start_publish(const QItemSelection &selected, const QItemSelection &deselected)
 {
-    ui->listView_startNode_subscribe->selectionModel()->clearSelection();
-    ui->listView_endNode_publish->selectionModel()->clearSelection();
+    ui->tableView_startNode_subscribe->selectionModel()->clearSelection();
+    ui->tableView_endNode_publish->selectionModel()->clearSelection();
     QModelIndex index;
-    index =  ui->listView_startNode_publish->selectionModel()->currentIndex();
+    index =  ui->tableView_startNode_publish->selectionModel()->currentIndex();
     fromString=(index.data(Qt::DisplayRole).toString());
-    int chopIndex = fromString.indexOf("{");
-    fromString.chop(fromString.count()-chopIndex);
-
     updateFrom();
 }
 
 
 void RemapArrowEdit::selectionHandle_end_subscribe(const QItemSelection &selected, const QItemSelection &deselected)
 {
-    ui->listView_startNode_subscribe->selectionModel()->clearSelection();
-    ui->listView_endNode_publish->selectionModel()->clearSelection();
+    ui->tableView_startNode_subscribe->selectionModel()->clearSelection();
+    ui->tableView_endNode_publish->selectionModel()->clearSelection();
     QModelIndex index;
-    index =  ui->listView_endNode_subscribe->selectionModel()->currentIndex();
+    index =  ui->tableView_endNode_subscribe->selectionModel()->currentIndex();
     toString =( index.data(Qt::DisplayRole).toString());
-    int chopIndex = toString.indexOf("{");
-    toString.chop(toString.count()-chopIndex);
     updateTo();
 }
 
@@ -148,9 +195,9 @@ void RemapArrowEdit::accept() {
                                  QString::fromUtf8("<h2>Missing remap information</h2>"
                                                    "<p>Please select a row on both sides or fill in the right information.</p>"));
     } else{
-        if (ui->listView_startNode_subscribe->selectionModel()->currentIndex().isValid())
+        if (ui->tableView_startNode_subscribe->selectionModel()->currentIndex().isValid())
             subToPub=false;
-        if (ui->listView_startNode_publish->selectionModel()->currentIndex().isValid())
+        if (ui->tableView_startNode_publish->selectionModel()->currentIndex().isValid())
             subToPub=true;
 
         QDialog::accept();
